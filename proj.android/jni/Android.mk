c39daf83efbfe9a5b239f3f1bffdd7b8717a7709
Wilson E. Alvarez <wilson.e.alvarez1@gmail.com>
2017-07-13 10:26:10 -0400

    Updated makefiles

---
+++ proj.android/jni/Android.mk
@@ -0,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos/audio/include)
+
+LOCAL_MODULE := MyGame_shared
+
+LOCAL_MODULE_FILENAME := libMyGame
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+	../../Classes/AppDelegate.cpp \
+	../../Classes/HelloWorldScene.cpp \
+	../../Classes/CCImGui/ImGuiManager.cpp \
+	../../Classes/CCImGui/ImGuiLayer.cpp \
+	../../Classes/CCImGui/imgui/imgui.cpp \
+	../../Classes/CCImGui/imgui/imgui_demo.cpp \
+	../../Classes/CCImGui/imgui/imgui_draw.cpp \
+	../../Classes/CCImGui/platform/imgui_impl_desktop.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+# _COCOS_HEADER_ANDROID_BEGIN
+# _COCOS_HEADER_ANDROID_END
+
+
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
+# _COCOS_LIB_ANDROID_BEGIN
+# _COCOS_LIB_ANDROID_END
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
+
+# _COCOS_LIB_IMPORT_ANDROID_BEGIN
+# _COCOS_LIB_IMPORT_ANDROID_END
